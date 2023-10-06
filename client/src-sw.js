1e5f12ec2423a02dec32b5fa4dfc37c01d1fce3c
Coral <cnight18@hotmail.com>
2023-10-06 09:53:15 -0300

    working thought todo sections

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,12 +1,13 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+// Workbox libraries
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Webpack manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// cacheFirst strategy for page caching
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,17 +15,32 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, 
     }),
   ],
 });
 
+// specific URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// navigation requests 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// TODO:Implement caching for other assets (e.g., JS, CSS, images, fonts)
+registerRoute(
+  /\.(?:js|css|jpg|jpeg|png|gif|svg|ico|woff2)$/,
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, 
+      }),
+    ],
+  })
+);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,28 @@
-const butInstall = document.getElementById('buttonInstall');
-
-// Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const result = await deferredPrompt.userChoice;
+    if (result.outcome === 'accepted') {
+      console.log('PWA installation accepted.');
+    } else {
+      console.log('PWA installation declined.');
+    }
+    deferredPrompt = null;
+    butInstall.style.display = 'none';
+  }
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA installed successfully.');
+});

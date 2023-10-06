a394b2858b3572180061814f0e60a1067e60085d
Coral <cnight18@hotmail.com>
2023-10-06 10:30:30 -0300

    constant errors with images , npm

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,66 +5,65 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
-      }),
-      new WebpackPwaManifest({
-        name: 'J.A.T.E', 
-        short_name: 'JATE', 
-        description: 'Just Another Text Editor',
-        background_color: '#272822', 
-        theme_color: '#31a9e1', 
-        start_url: './', 
-        icons: [
-          {
-            src: path.resolve('src/assets/icons/icon_96x96.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'sw.js',
-      }),
-    ],
-    module: {
-      rules: [
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/js/index.js",
+    install: "./src/js/install.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "JATE",
+    }),
+    new InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "src-sw.js",
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: "Just Another Text Editor",
+      short_name: "J.A.T.E.",
+      description:
+        "Just Another Text Editor' or 'JATE' for short.",
+      background_color: "#225ca3",
+      theme_color: "#225ca3",
+      start_url: "/",
+      publicPath: "/",
+      icons: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
         },
       ],
-    },
-  };
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/transform-runtime",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
-

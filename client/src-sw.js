bb07fe4f854a01a51b8b741df829d85d6b8c01d6
Coral <cnight18@hotmail.com>
2023-10-06 10:34:59 -0300

    everything is a mess

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,13 +1,14 @@
 // Workbox libraries
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
+import { precacheAndRoute } from "workbox-precaching";
+import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
+import { registerRoute } from "workbox-routing";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { ExpirationPlugin } from "workbox-expiration";
 
 // Webpack manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
-// cacheFirst strategy for page caching
+// Cache for page caching (e.g., HTML pages)
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -20,16 +21,10 @@ const pageCache = new CacheFirst({
   ],
 });
 
-// specific URLs
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-// navigation requests 
+// Cache specific URLs for offline use
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO:Implement caching for other assets (e.g., JS, CSS, images, fonts)
+// Cache other assets (e.g., JS, CSS, images, fonts)
 registerRoute(
   /\.(?:js|css|jpg|jpeg|png|gif|svg|ico|woff2)$/,
   new StaleWhileRevalidate({
@@ -39,8 +34,19 @@ registerRoute(
         statuses: [0, 200],
       }),
       new ExpirationPlugin({
-        maxAgeSeconds: 7 * 24 * 60 * 60, 
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       }),
     ],
   })
 );
+
+// errors 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/offline.html'); //offline fallback page
+      });
+    })
+  );
+});
